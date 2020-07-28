58fd89997c63990d7a626f8a8a21e103b94176d6
Jayavardhani Kathika <jayavardhani.kathika@gmail.com>
2020-07-28 16:24:33 -0700

    Initial commit

---
+++ src/main/java/com/project/shopmart/ShopmartApplication.java
@@ -0,0 +1,13 @@
+package com.project.shopmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmartApplication.class, args);
+	}
+
+}
